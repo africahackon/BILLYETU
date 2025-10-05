9dbfae4b684d606fc4651f7fc81fb4a31ad26ca8
LDNSNET <ldnsnetworks@gmail.com>
2025-10-05 19:07:10 +0300

    new

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,7 @@ import VueApexCharts from "vue3-apexcharts";
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
-import { ZiggyVue } from 'ziggy';
+import { ZiggyVue } from 'ziggy-js';
 
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
